18b4717455152e978ab2199199fd46b13c0cc6f8
Marzie79 <marzie.7900@gmail.com>
2024-06-11 21:56:06 +0330

    test

---
+++ First.java
@@ -0,0 +1,16 @@
+public class First{
+
+    static int sum(int first, int second){
+        return first + second;
+    }
+    
+    public static void main(String[] args) {
+        // System.out.println("Hello java :)");
+        // System.out.print("Test");
+        if (false)
+            System.out.print("Test");
+        else
+            System.out.print(sum(1, 2));
+
+    }
+}
